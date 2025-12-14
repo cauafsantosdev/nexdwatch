5b6a1544abd1c8b6b17c3876d961aed928c1a04c
cauafsantosdev <cauafsantosdev@gmail.com>
2025-12-14 11:50:06 -0300

    refactor: add total_logs treshold for a film on queue to be inserted to db

--- app/scraper/film_scraping.py
+++ app/scraper/film_scraping.py
@@ -9,24 +9,28 @@ def scrape_film_queue(films: list):
     for slug in films:
         film = client.get_film(slug)
 
-        films_data.append({
-            "id": film.id,
-            "slug": slug,
-            "title": film.title,
-            "original_title": film.original_title,
-            "year": film.year,
-            "runtime": film.runtime,
-            "director": film.director,
-            "genre": film.genre,
-            "country": film.country,
-            "language": film.language,
-            "actors": film.actors,
-            "studio": film.studio,
-            "synopsis": film.synopsis,
-            "tagline": film.tagline,
-            "themes": film.themes,
-            "avg_rating": film.avg_rating,
-            "total_logs": film.total_logs
-        })
+        total_logs = film.total_logs
+
+        if total_logs >= 1000:
+            
+            films_data.append({
+                "id": film.id,
+                "slug": slug,
+                "title": film.title,
+                "original_title": film.original_title,
+                "year": film.year,
+                "runtime": film.runtime,
+                "director": film.director,
+                "genre": film.genre,
+                "country": film.country,
+                "language": film.language,
+                "actors": film.actors,
+                "studio": film.studio,
+                "synopsis": film.synopsis,
+                "tagline": film.tagline,
+                "themes": film.themes,
+                "avg_rating": film.avg_rating,
+                "total_logs": film.total_logs
+            })
 
     return films_data
